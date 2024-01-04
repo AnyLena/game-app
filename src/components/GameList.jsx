c29b94775cd1e4becc2ba693dd7f6a513f53897c
LenaBultmann <lena.bultmann@gmx.de>
2024-01-04 09:34:02 +0100

    adding delete buttons

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -3,14 +3,6 @@ import { useState, useEffect } from "react";
 const GameList = ({ games, setGames }) => {
   const [nextGame, setNextGame] = useState();
 
-  const handleNext = () => {
-    const unplayedGames = games.filter( game => game.played === false);
-    if (unplayedGames.length > 0) {
-        let next = Math.floor(Math.random() * unplayedGames.length);
-        setNextGame(unplayedGames[next].id);
-    }
-  };
-
   useEffect(() => {
     if (nextGame !== undefined) {
       setGames(
@@ -21,6 +13,18 @@ const GameList = ({ games, setGames }) => {
     }
   }, [nextGame]);
 
+  const handleNext = () => {
+    const unplayedGames = games.filter( game => game.played === false);
+    if (unplayedGames.length > 0) {
+        let next = Math.floor(Math.random() * unplayedGames.length);
+        setNextGame(unplayedGames[next].id);
+    }
+  };
+
+const handleDelete = () => {
+    console.log('delete')
+}
+
   return (
     <>
       <div className="container">
@@ -29,7 +33,8 @@ const GameList = ({ games, setGames }) => {
             className={game.id === nextGame ? "card this-card" : "card"}
             key={game.id}
           >
-            {game.name} {game.played ? "true" : "false"}
+            {game.name}
+            <button onClick={handleDelete} className="delete-btn">X</button>
           </div>
         ))}
       </div>
